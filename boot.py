0a4c7eef48863b8720dc3985878a58aa139ca18a
Robin <robinadhikari777@gmail.com>
2025-07-31 13:57:05 -0700

    added comments for library issue

--- boot.py
+++ boot.py
@@ -1,4 +1,5 @@
-import usb_cdc
+import usb_cdc #this is a library for CircuitPython which runs on the Adafruit Microcontroller
+# it will show it as a problem because VSCode doesn't recognize the library since it is for CircuitPython and only runs on the Microcontroller itself.
 
 # Sets up USB CDC to let microcontroller act as a USB serial device
 usb_cdc.enable(console=False, data=True) # Disables REPL and enables an extra port to receive/send data
